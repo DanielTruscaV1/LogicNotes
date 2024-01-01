b2da1e82344f62a5ee355d1b6e4cc18eee5fba97
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-01-01 21:50:42 +0200

    Added Google Fonts to the project.

--- logicnotes/src/components/Navigation.vue
+++ logicnotes/src/components/Navigation.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-  
+  <nav>
+
+  </nav>
 </template>
 
 <style scoped>
-
+    nav 
+    {
+        width:100vw;
+        height:10vh;
+        background-color:rgb(40, 10, 70);
+        color:white;
+    }
 </style>
