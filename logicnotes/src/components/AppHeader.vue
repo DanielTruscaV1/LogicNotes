5b6a9bbc6943c0239b89841e73046154c34038d9
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-01-01 22:10:06 +0200

    Created some UI for the Header and Navigation Components.

--- logicnotes/src/components/AppHeader.vue
+++ logicnotes/src/components/AppHeader.vue
@@ -3,9 +3,32 @@
 </script>
 
 <template>
-  
+  <header>
+    <h1>
+      LogicNotes
+    </h1>
+  </header>
 </template>
 
 <style scoped>
-
+    header 
+    {
+      position:absolute;
+      top:0px;
+      left:0px;
+      width:100vw;
+      height:10vh;
+      background-color:rgb(40, 10, 70);
+      color:white;
+      font-family: 'Montserrat', sans-serif;
+    }
+    h1 
+    {
+      position:absolute;
+      top:15px;
+      left:20px;
+      margin:0px;
+      text-align:left;
+      font-size:30px;
+    }
 </style>
--- logicnotes/src/components/AppNavigation.vue
+++ logicnotes/src/components/AppNavigation.vue
@@ -5,18 +5,53 @@
 <template>
   <nav>
     <h1>
-        
+       Navigation 
     </h1>
+    <ul>
+      <li>Home</li>
+      <li>Notes</li>
+      <li>Sign-in</li>
+      <li>Sign-up</li>
+      <li>Account</li>
+      <li>Settings</li>
+    </ul>
   </nav>
 </template>
 
 <style scoped>
     nav 
     {
-        width:100vw;
-        height:10vh;
-        background-color:rgb(40, 10, 70);
-        color:white;
-        font-family: 'Montserrat', sans-serif;
+      position:absolute;
+      top:10vh;
+      left:0px;
+      width:100vw;
+      height:10vh;
+      background-color:rgb(40, 10, 70);
+      color:white;
+      font-family: 'Montserrat', sans-serif;
+    }
+    h1 
+    {
+      text-align:center;
+    }
+    ul 
+    {
+      display:none;
+      position:relative;
+      top:0px;
+      margin:0px;
+      padding:0px;
+      height:50vh;
+    }
+    li 
+    {
+      position:relative;
+      margin-left:0px;
+      padding-top:20px;
+      height:50px;
+      list-style-type:none;
+      text-align:center;
+      font-size:20px;
+      background-color:rgb(60, 30, 90);
     }
 </style>
