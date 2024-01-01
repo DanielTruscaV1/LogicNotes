769df77cdad83f6c564690f1d2309a14ec4e4bf2
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-01-01 23:00:18 +0200

    Added a new Logo.

--- logicnotes/src/components/AppPage.vue
+++ logicnotes/src/components/AppPage.vue
@@ -7,6 +7,7 @@
     <h1>
       Welcome to the LogicNotes project!
     </h1>
+    <img src="../assets/Logo1.png"/>
   </main>
 </template>
 
@@ -26,5 +27,14 @@
     {
       font-size:20px;
       text-align:center;
+    }
+    img
+    {
+      position:relative;
+      top:30px;
+      left:10vw;
+      width:80vw;
+      height:40vh;
+      border-radius:50px;
     }
 </style>
