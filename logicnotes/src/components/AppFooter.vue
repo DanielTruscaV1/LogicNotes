e4e8edf53627834b1dca887826e758816d0156c1
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-01-01 22:49:03 +0200

    Created some UI for the footer component.

--- logicnotes/src/components/AppFooter.vue
+++ logicnotes/src/components/AppFooter.vue
@@ -3,9 +3,30 @@
 </script>
 
 <template>
-  
+  <footer>
+    <h1>
+        © LogicNotes 2024 Copyright
+    </h1>
+  </footer>
 </template>
 
 <style scoped>
-
+    footer
+    {
+        position:absolute;
+        top:90vh;
+        width:100vw;
+        height:10vh;
+        background-color:rgb(40, 10, 70);
+        color:white;
+        font-family: 'Montserrat', sans-serif;
+        z-index:2;
+    }
+    h1 
+    {
+        margin:0px;
+        padding-top:20px;
+        font-size:20px;
+        text-align:center;
+    }
 </style>
