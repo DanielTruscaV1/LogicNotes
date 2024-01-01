96bb8dc767f14deeae8560681d5c5b18ed936129
DanielTruscaV1 <danieltrusca2016@gmail.com>
2024-01-01 21:47:02 +0200

    Created a set of Vue components for the Navigation, Page and Footer sections of the website.

--- logicnotes/src/App.vue
+++ logicnotes/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+    import Navigation from "./components/Navigation.vue";
+    import Page from "./components/Page.vue";
+    import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  
+  <Navigation/>
+  <Page/>  
+  <Footer/>
 </template>
 
 <style scoped>
