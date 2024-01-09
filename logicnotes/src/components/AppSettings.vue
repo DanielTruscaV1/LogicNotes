db087a01353576cff8b76203d356358f6d414a63
DanielTruscaV1 <danieltrusca2008@gmail.com>
2024-01-09 14:49:00 +0200

    Created the theme picker feature.

--- logicnotes/src/components/AppSettings.vue
+++ logicnotes/src/components/AppSettings.vue
@@ -1,4 +1,31 @@
 <script setup>
+    function changeTheme(theme)
+    {
+        if(theme == "black")
+        {
+            document.documentElement.style.setProperty('--theme1', 'rgb(40, 40, 40)');
+            document.documentElement.style.setProperty('--theme2', 'rgb(20, 20, 20)');
+            document.documentElement.style.setProperty('--theme3', 'rgb(10, 10, 10)');
+            document.documentElement.style.setProperty('--color', 'white');
+            document.documentElement.style.setProperty('--color2', 'black');
+        }
+        else if(theme == "blue")
+        {
+            document.documentElement.style.setProperty('--theme1', 'rgb(230, 230, 255)');
+            document.documentElement.style.setProperty('--theme2', 'rgb(200, 200, 255)');
+            document.documentElement.style.setProperty('--theme3', 'rgb(180, 180, 235)');
+            document.documentElement.style.setProperty('--color', 'black');
+            document.documentElement.style.setProperty('--color2', 'white');
+        }
+        else if(theme == "orange")
+        {
+            document.documentElement.style.setProperty('--theme1', 'rgb(255, 220, 170)');
+            document.documentElement.style.setProperty('--theme2', 'rgb(255, 200, 150)');
+            document.documentElement.style.setProperty('--theme3', 'rgb(235, 170, 120)');
+            document.documentElement.style.setProperty('--color', 'black');
+            document.documentElement.style.setProperty('--color2', 'white');
+        }
+    }
 </script>
 
 <template>
@@ -11,13 +38,13 @@
                 <button id="theme-button1">
 
                 </button>
-                <button id="theme-button2">
+                <button id="theme-button2" @click="changeTheme('black')">
                     
                 </button>
-                <button id="theme-button3">
+                <button id="theme-button3" @click="changeTheme('blue')">
                     
                 </button>
-                <button id="theme-button4">
+                <button id="theme-button4" @click="changeTheme('orange')">
                     
                 </button>
             </section>
